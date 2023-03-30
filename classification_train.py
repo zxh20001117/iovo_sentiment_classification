5b36f61395a060594516dcb7c205dfb754945aab
F4riK1ller <287961528@qq.com>
2023-03-30 11:16:57 +0800

    baisc train model

--- classification_train.py
+++ classification_train.py
@@ -1,14 +1,24 @@
+import numpy as np
 import torch
 import torch.nn as nn
+from transformers import BertModel
+from configparser import ConfigParser
+import torch.nn.functional as F
 
+from generate_bert_vector import data_split, get_data, get_iovo_data
+
+conf = ConfigParser()
+conf.read("config.ini", encoding='UTF-8')
 USE_CUDA = torch.cuda.is_available()
+
+
 class bert_lstm(nn.Module):
     def __init__(self,
-                 hidden_dim,
-                 output_size,
-                 n_layers,
-                 bidirectional=True,
-                 drop_prob=0.5):
+                 hidden_dim=conf.getint("train", "hidden_dim"),
+                 output_size=conf.getint("train", "output_size"),
+                 n_layers=conf.getint("train", "n_layers"),
+                 bidirectional=conf.getboolean("train", "bidirectional"),
+                 drop_prob=conf.getfloat("train", "drop_prob")):
         super(bert_lstm, self).__init__()
 
         self.output_size = output_size
@@ -17,24 +27,33 @@ class bert_lstm(nn.Module):
         self.bidirectional = bidirectional
 
         # Bert ----------------重点，bert模型需要嵌入到自定义模型里面
-        self.bert = BertModel.from_pretrained("../chinese-bert_chinese_wwm_pytorch/data")
+        self.bert = BertModel.from_pretrained(conf.get("train", "pretrained_model"))
         for param in self.bert.parameters():
             param.requires_grad = True
 
         # LSTM layers
-        self.lstm = nn.LSTM(768, hidden_dim, n_layers, batch_first=True, bidirectional=bidirectional)
+        self.lstm = nn.LSTM(768, hidden_dim,
+                            conf.getint("train", "n_layers"),
+                            batch_first=True,
+                            bidirectional=conf.getboolean("train", "bidirectional")
+                            )
 
         # dropout layer
         self.dropout = nn.Dropout(drop_prob)
 
+
         # linear and sigmoid layers
         if bidirectional:
-            self.fc = nn.Linear(hidden_dim * 2, output_size)
+            self.fc1 = nn.Linear(hidden_dim * 2, conf.getint("train", "full_con1_out"))
         else:
-            self.fc = nn.Linear(hidden_dim, output_size)
+            self.fc1 = nn.Linear(hidden_dim, conf.getint("train", "full_con1_out"))
+
+        self.act = nn.ReLU()
+
+        self.fc2 = nn.Linear(conf.getint("train", "full_con1_out"), 2)
 
         # self.sig = nn.Sigmoid()
-
+        self.softmax = nn.Softmax()
     def forward(self, x, hidden):
         batch_size = x.size(0)
         # 生成bert字向量
@@ -64,9 +83,15 @@ class bert_lstm(nn.Module):
         # dropout and fully-connected layer
         out = self.dropout(hidden_last_out)
         # print(out.shape)    #[32,768]
-        out = self.fc(out)
+        out = self.fc1(out)
+        out = self.act(out)
 
+        out = self.fc2(out)
         return out
+
+        # out = F.softmax(out, dim=-1)
+        # return out
+
 
     def init_hidden(self, batch_size):
         weight = next(self.parameters()).data
@@ -86,5 +111,61 @@ class bert_lstm(nn.Module):
 
         return hidden
 
-if __name__ == "__main__" :
-    print()
+
+if __name__ == "__main__":
+    model = bert_lstm()
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=conf.getfloat("train", "lr"))
+
+    if USE_CUDA: model.cuda()
+
+    model.train()
+
+    # train for some number of epochs
+    epochs = conf.getint("train", "epochs")
+    data = get_data("sentimentData/marked sentences sentiment 2.xlsx")
+    data = get_iovo_data(data, 1, 2)
+    train_loader, valid_loader, test_loader = data_split(data)
+
+    for e in range(epochs):
+        # initialize hidden state
+        h = model.init_hidden(conf.getint("train", "batch_size"))
+        counter = 0
+        print_every = conf.getint("train", "print_every")
+        batch_size = conf.getint("train", "batch_size")
+
+        # batch loop
+        for inputs, labels in train_loader:
+            counter += 1
+
+            if USE_CUDA:
+                inputs, labels = inputs.cuda(), labels.cuda()
+            h = tuple([each.data for each in h])
+            model.zero_grad()
+            output = model(inputs, h)
+            loss = criterion(output, labels)
+            loss.backward()
+            optimizer.step()
+
+            # loss stats
+            if counter % print_every == 0:
+                model.eval()
+                with torch.no_grad():
+                    val_h = model.init_hidden(batch_size)
+                    val_losses = []
+                    for inputs, labels in valid_loader:
+                        val_h = tuple([each.data for each in val_h])
+
+                        if (USE_CUDA):
+                            inputs, labels = inputs.cuda(), labels.cuda()
+
+                        output = model(inputs, val_h)
+                        val_loss = criterion(output, labels)
+
+                        val_losses.append(val_loss.item())
+
+                model.train()
+                print("Epoch: {}/{}...".format(e + 1, epochs),
+                      "Step: {}...".format(counter),
+                      "Loss: {:.6f}...".format(loss.item()),
+                      "Val Loss: {:.6f}".format(np.mean(val_losses)))
--- generate_bert_vector.py
+++ generate_bert_vector.py
@@ -7,6 +7,9 @@ import torch
 from configparser import ConfigParser
 from transformers import BertTokenizer
 
+from torch.utils.data import TensorDataset, DataLoader
+from sklearn.model_selection import train_test_split
+
 conf = ConfigParser()
 conf.read("config.ini", encoding='UTF-8')
 
@@ -39,7 +42,7 @@ torch.cuda.manual_seed_all(conf.getint("train", "seed_val"))
 
 def get_data(path):
     startTime = time.time()
-    df = pd.read_excel(path)
+    df = pd.read_excel(path).reset_index()
     loadTime = time.time()
     print(f'it takes {int(loadTime - startTime)} s for loading data\n')
 
@@ -48,7 +51,7 @@ def get_data(path):
                                           do_lower_case=conf.getboolean("train", "do_lower_case"))
     df["sent_bert_token_length"] = df["sentence"].apply(
         lambda x: len(tokenizer(x, add_special_tokens=conf.getboolean("train", "add_special_tokens"))["input_ids"]))
-    encoded_data = tokenizer.batch_encode_plus(
+    token_data = tokenizer.batch_encode_plus(
         df.sentence.values,
         add_special_tokens=conf.getboolean("train", "add_special_tokens"),
         return_attention_mask=conf.getboolean("train", "return_attention_mask"),
@@ -56,8 +59,8 @@ def get_data(path):
         max_length=conf.getint("train", "seq_length"),
         return_tensors=conf.get("train", "return_tensors")
     )
-    df["input_ids"] = encoded_data["input_ids"]
-    df['attention_mask'] = encoded_data['attention_mask']
+    tokens = pd.DataFrame({'token': list(token_data['input_ids'].numpy())})
+    df = pd.concat([df, tokens], axis=1)
     encodingTime = time.time()
     print(f'it takes {int(encodingTime - loadTime)} s for BertTokenizer and Encoding\n')
 
@@ -65,11 +68,48 @@ def get_data(path):
 
 
 def get_iovo_data(data, level_one, level_two):
-    return data[data["sentiment"] == level_one | data["sentiment"] == level_two]
+    iovo_data = data[(data["sentiment"] == level_one) | (data["sentiment"] == level_two)].copy()
+    iovo_data['label'] = iovo_data.apply(lambda x: 1 if x['sentiment'] == level_one else 0, axis = 1)
+    return iovo_data
+
+
+def data_split(data):
+    data = data.copy().reset_index()
+    startTime = time.time()
+    X = torch.tensor(np.array([i for i in data['token']]))
+    y = torch.from_numpy(data['label'].values).long()
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                        test_size=conf.getfloat("data_split", "test_size"),
+                                                        shuffle=True,
+                                                        stratify=y,
+                                                        random_state=conf.getint("data_split", "random_state"))
+
+    X_valid, X_test, y_valid, y_test = train_test_split(X_test, y_test,
+                                                        test_size=conf.getfloat("data_split", "valid_size"),
+                                                        shuffle=True,
+                                                        stratify=y_test,
+                                                        random_state=conf.getint("data_split", "random_state"))
+
+    # create Tensor datasets
+    train_data = TensorDataset(X_train, y_train)
+    valid_data = TensorDataset(X_valid, y_valid)
+    test_data = TensorDataset(X_test, y_test)
+
+    # dataloaders
+    batch_size = conf.getint("train", "batch_size")
+
+    # make sure the SHUFFLE your training data
+    train_loader = DataLoader(train_data, shuffle=True, batch_size=batch_size, drop_last=True)
+    valid_loader = DataLoader(valid_data, shuffle=True, batch_size=batch_size, drop_last=True)
+    test_loader = DataLoader(test_data, shuffle=True, batch_size=batch_size, drop_last=True)
+    endTime = time.time()
+    print(f'it takes {int(endTime - startTime)} s for splitting and encapsulating data\n')
+
+    return train_loader, valid_loader, test_loader
 
 
 if __name__ == "__main__":
     data = get_data("sentimentData/marked sentences sentiment 2.xlsx")
     print(len(data))
-    print("\ninput_ids:\n", data.head(20)['input_ids'])
-    print("\nattention_mask:\n", data.head(20)['attention_mask'])
+    data = get_iovo_data(data, 1, 2)
